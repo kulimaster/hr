c0b91f8d0f3c58035268d54323a9f4c0f4715cfa
Martin Jermar <mjermar@csas.cz>
2025-09-19 11:53:23 +0200

    add automapper, mediatr

---
+++ HR.LeaveManagement/HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(cfg => { }, Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        return services;
+    }
+}
---
+++ HR.LeaveManagement/HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
@@ -0,0 +1,3 @@
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public record LeaveTypeDto(string Name, int DefaultDays);
